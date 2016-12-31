ded7e85882dcce9fd5ab66dcadd76477c4e7ec90
Adam Krusic <Adam Krusic>
2016-12-31 01:05:47 -0700

    made create an internal function

--- model/button.py
+++ model/button.py
@@ -7,9 +7,12 @@ from model.image import Image
 class Button:
 
     def __init__(self, text, x, y, background_image_path):
-        self.create(text, Location(x, y), Image(background_image_path))
+        location = Location(x, y)
+        image = Image(background_image_path)
 
-    def create(self, text, location, background):
+        self._create(text, location, image)
+
+    def _create(self, text, location, background):
         self.text = text
         self.location = location
         self.background = background
