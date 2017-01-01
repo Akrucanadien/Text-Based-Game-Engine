c7ff3aded87e3231f876236295000249300dc2b5
Adam Krusic <Adam Krusic>
2016-12-31 18:11:28 -0700

    added dialogue class

---
+++ model/dialogue.py
@@ -0,0 +1,15 @@
+# Adam Krusic
+
+from model.image import Image
+from model.location import Location
+
+
+class Dialogue:
+
+    def __init__(self, text, x, y, background_file_path):
+        location = Location(x, y)
+        background = Image(background_file_path)
+        self._create(text, location, background)
+
+    def _create(self, text, location, background):
+        
